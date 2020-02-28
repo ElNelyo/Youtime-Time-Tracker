aa9b763fda07543deef05b16095e26e98887a63f
Charly <charly.strn@gmail.com>
2020-02-28 22:34:03 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
+  important: true,
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      margin: {
+        '96': '24rem',
+        '128': '32rem',
+      },
+    }
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    opacity: ['responsive', 'hover']
+  }
 }
