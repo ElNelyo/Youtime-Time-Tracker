b79d6dbb2a089e213c9c251ff0cc10b7dc20a98a
Charly <charly.strn@gmail.com>
2020-03-02 11:31:03 +0100

    add

--- src/preload.ts
+++ src/preload.ts
@@ -6,6 +6,7 @@ const Store = require('electron-store');
 const store = new Store();
 
 
+
 window.addEventListener("DOMContentLoaded", async() => {
 
 
@@ -101,6 +102,7 @@ async function onChange() {
     
     var issuesList = document.createElement("select");
     issuesList.id = "issuesList";
+    issuesList.className ="block max-w-2xl appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500";
     myParent.appendChild(issuesList);
 
     issues.forEach(function(item: any){  
